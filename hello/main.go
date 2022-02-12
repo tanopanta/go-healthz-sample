ab56dfafe5c3d6d2689a0a48690c6d0f0ebbffea
tanopanta <25007602+tanopanta@users.noreply.github.com>
2022-02-13 00:24:11 +0900

    add healthz

--- hello/main.go
+++ hello/main.go
@@ -11,6 +11,8 @@ import (
 func main() {
 	api := gin.Default()
 	api.GET("/", helloWorld)
+	api.GET("/healthz", healthCheck)
+
 	api.Run(":3000")
 }
 
@@ -33,4 +35,25 @@ func helloWorld(c *gin.Context) {
 	}
 
 	c.String(http.StatusOK, "hello "+message)
+}
+
+func healthCheck(c *gin.Context) {
+	client := resty.New()
+
+	var message string
+	for _, port := range []string{"4000", "4001", "4002", "4003", "4004"} {
+		resp, err := client.R().
+			EnableTrace().
+			Get(fmt.Sprintf("http://localhost:%s", port))
+		if err != nil {
+			c.String(
+				http.StatusServiceUnavailable,
+				fmt.Sprintf("Unexpected error occurred.: %+v", err),
+			)
+			return
+		}
+		message += resp.String()
+	}
+
+	c.String(http.StatusOK, "OK.")
 }
